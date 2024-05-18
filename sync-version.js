21b98dd539288a5ded7d38c300566d9b43a20f53
Yauhen Pyl <yauhen.pyl@gmail.com>
2024-05-18 17:30:58 +0200

    no update

--- sync-version.js
+++ sync-version.js
@@ -10,17 +10,17 @@ const main = async () => {
     // Get the content of the JSON file
     const packageJsonData = await fsPromises.readFile(packageJsonPath)
     const neuConfigData = await fsPromises.readFile(neuConfigPath)
-    const updateData = await fsPromises.readFile(updatePath)
+    //const updateData = await fsPromises.readFile(updatePath)
 
     // Turn it to an object
     const packageJsonObj = JSON.parse(packageJsonData)
     const neeJsonObj = JSON.parse(neuConfigData)
-    const updateObj = JSON.parse(updateData)
+    //const updateObj = JSON.parse(updateData)
 
     // Do something with the result
     console.log(packageJsonObj.version)
     neeJsonObj.version = packageJsonObj.version
-    updateObj.version = packageJsonObj.version
+    //updateObj.version = packageJsonObj.version
     await fsPromises.writeFile(neuConfigPath, JSON.stringify(neeJsonObj, null, 2));
     await fsPromises.writeFile(updatePath, JSON.stringify(updateObj, null, 2));
   } catch (err) {
